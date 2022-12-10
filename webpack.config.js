7cc9da9485adc6c2514d0624d46618582e26ddd6
Andrew Belyaev <andrew.belyaev671@gmail.com>
2022-12-10 13:37:47 +0300

    start position

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-first-webpack.bundle.js',
+    },
+    watch: true,
+
+};
